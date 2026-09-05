2d6ccc932c7e35901fe684cf908afbd168c66ed2
Ilya Sovtsov <ilyasovty@gmail.com>
2026-09-05 10:30:21 +0700

    prettification

--- calculator.scala
+++ calculator.scala
@@ -2,7 +2,7 @@ import scala.util.boundary
 import scala.util.boundary.break
 
 @main def calculator(commands: String*): Unit = {
-  var calc = Calc(0, 0, 0, false)
+  var calc = Calc.zeroed
 
   boundary:
     for cmd <- commands do {
@@ -13,23 +13,27 @@ import scala.util.boundary.break
         case "/"     => calc.div
         case "swap"  => calc.swap
         case "blink" => calc.blink
-        case "acc"   => calc.acc
+        case "acc"   => calc.load(calc.acc)
         case "break" => break()
         case s       => calc.load(s.toInt)
     }
 
-  println(calc.accumulator)
+  println(calc.acc)
 }
 
-case class Calc(accumulator: Int, a: Int, b: Int, flag: Boolean) {
-  def add: Calc   = copy(accumulator = a + b, flag = false)
-  def sub: Calc   = copy(accumulator = a - b, flag = false)
-  def mul: Calc   = copy(accumulator = a * b, flag = false)
-  def div: Calc   = if b != 0 then copy(accumulator = a / b, flag = false) else Calc(0, 0, 0, false)
-  def swap: Calc  = copy(a = b, b = a)
-  def blink: Calc = copy(flag = !flag)
-  def acc: Calc   = load(accumulator)
-  def load(x: Int): Calc =
-    val loaded = if !flag then copy(a = x) else copy(b = x)
-    loaded.blink
+object Calc {
+  def zeroed: Calc = Calc(0, 0, 0, false)
+}
+
+case class Calc(acc: Int, a: Int, b: Int, flag: Boolean) {
+  def add: Calc = copy(acc = a + b).clearFlag
+  def sub: Calc = copy(acc = a - b).clearFlag
+  def mul: Calc = copy(acc = a * b).clearFlag
+  def div: Calc = if b != 0 then copy(acc = a / b).clearFlag else Calc.zeroed
+
+  def blink: Calc     = copy(flag = !flag)
+  def clearFlag: Calc = copy(flag = false)
+
+  def swap: Calc = copy(a = b, b = a)
+  def load(x: Int): Calc = (if !flag then copy(a = x) else copy(b = x)).blink
 }
